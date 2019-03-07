419da09d9093aaead24be521d159de7487cae807
dennewbie <caruso.denny@gmail.com>
2019-03-07 09:55:13 +0100

    Initial Commit

---
+++ NanoChallenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoChallenge2
+//
+//  Created by Denny Caruso on 07/03/2019.
+//  Copyright © 2019 Denny Caruso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
